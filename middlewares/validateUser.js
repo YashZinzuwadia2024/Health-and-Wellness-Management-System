7c9ef0e483eec1559cac6e3e4d171f9cd2d6c0f5
YashZinzuwadia2024 <yash.zinzuwadia.2024@gmail.com>
2024-06-03 19:28:56 +0530

    Change in session middleware

--- middlewares/validateUser.js
+++ middlewares/validateUser.js
@@ -1,12 +1,17 @@
 const notifier = require('node-notifier')
 
 const sessionChecker = async (req, res, next) => {
-    if (req.session.profile) return next();
-    notifier.notify({
-        title: "Session",
-        message: "Session Timed Out..Login Again!!"
-    });
-    return res.redirect("/");
+    if (req.session.profile) {
+        console.log(`Found User : ${req.session.profile.email.split('@')[0]}`)
+        return next();
+    }
+    else {
+        notifier.notify({
+            title: "Session",
+            message: "Session Timed Out..Login Again!!"
+        });
+        return res.redirect('/login');
+    }
 }
 
 module.exports = sessionChecker;
